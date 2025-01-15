bcbcf6cfb6fb03bbfe4592486e07d41e8dd61013
Nan72 <lecongnhan29@gmail.com>
2025-01-15 12:01:20 +0700

    feat(d5): complete part 2

--- src/days/5/Puzzle.ts
+++ src/days/5/Puzzle.ts
@@ -3,7 +3,7 @@ type Rule = {
   second: number;
 }
 
-const first = (input: string) => {
+function parseInput(input: string) : { rules: Rule[], updates: number[][] } {
   const splits = input.split('\n\n');
   const rules = splits[0]
     .split('\n')
@@ -20,25 +20,60 @@ const first = (input: string) => {
       .map(n=>parseInt(n))
     );
 
-  let result = 0;
-  for (const update of updates) {
-    let isUpdateOk = true;
-    for (const rule of rules) {
-      const firstIndex = update.indexOf(rule.first);
-      const secondIndex = update.indexOf(rule.second);
+  return { rules, updates };
+}
 
-      if (firstIndex === -1 || secondIndex === -1) {
-        continue;
-      }
+function isUpdateOk(update: number[], rules: Rule[]) {
+  let isUpdateOk = true;
+  for (const rule of rules) {
+    const firstIndex = update.indexOf(rule.first);
+    const secondIndex = update.indexOf(rule.second);
 
-      if (secondIndex >= firstIndex) {
-        continue;
-      }
-
-      isUpdateOk = false;
+    if (firstIndex === -1 || secondIndex === -1) {
+      continue;
     }
 
-    if (!isUpdateOk) {
+    if (secondIndex >= firstIndex) {
+      continue;
+    }
+
+    isUpdateOk = false;
+  }
+
+  return isUpdateOk;
+}
+
+// https://www.reddit.com/r/adventofcode/comments/1h71twj/2024_day_5_part_2_love_these_kind_of_days/
+function reOrderUpdate(update: number[], rules: Rule[]): number[] | undefined {
+  const _update = [...update];
+  for (const rule of rules) {
+    const firstIndex = _update.indexOf(rule.first);
+    const secondIndex = _update.indexOf(rule.second);
+
+    if (firstIndex === -1 || secondIndex === -1) {
+      continue;
+    }
+
+    if (secondIndex >= firstIndex) {
+      continue;
+    }
+
+    // swap first and second
+    const temp = _update[firstIndex];
+    _update[firstIndex] = _update[secondIndex];
+    _update[secondIndex] = temp;
+    // console.log('swapped', { 'first': _update[firstIndex], 'second': _update[secondIndex], update: _update });
+  }
+
+  return _update;
+}
+
+const first = (input: string) => {
+  const { rules, updates } = parseInput(input);
+
+  let result = 0;
+  for (const update of updates) {
+    if (!isUpdateOk(update, rules)) {
       continue;
     }
 
@@ -51,10 +86,32 @@ const first = (input: string) => {
 const expectedFirstSolution = 143;
 
 const second = (input: string) => {
-  input;
-  return 'solution 2';
+  const { rules, updates } = parseInput(input);
+
+  let result = 0;
+  for (const update of updates) {
+    let updateOk = isUpdateOk(update, rules);
+    if (updateOk) {
+      continue;
+    }
+
+    let reOrdered = [...update];
+    while (!updateOk) {
+       reOrdered = reOrderUpdate(reOrdered, rules);
+      if (!reOrdered) {
+        console.error('no re-ordering found', { update });
+        continue;
+      }
+
+      updateOk = isUpdateOk(reOrdered, rules);
+    }
+
+    result += reOrdered[Math.floor(reOrdered.length / 2)];
+  }
+
+  return result;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 123;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
